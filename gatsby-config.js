df966ffc806726df65bec6f447b257fe98fa3a25
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2019-10-18 10:06:11 +0200

    Adding ContentfulCMS account link

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -30,6 +33,14 @@ module.exports = {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
--- src/components/contact/contactUs.js
+++ src/components/contact/contactUs.js
@@ -7,8 +7,13 @@ const ContactUs = () => {
     <section className={styles.contact}>
       <Title title="contact" subtitle="us" />
       <div className={styles.center}>
-        <form action="" className={styles.form}>
+        <form
+          action="https://formspree.io/ashraf.hafiz@yahoo.com"
+          method="POST"
+          className={styles.form}
+        >
           <div>
+            <label htmlFor="name">name</label>
             <input
               type="text"
               name="name"
@@ -18,6 +23,7 @@ const ContactUs = () => {
             />
           </div>
           <div>
+            <label htmlFor="email">email</label>
             <input
               type="email"
               name="email"
@@ -27,6 +33,7 @@ const ContactUs = () => {
             />
           </div>
           <div>
+            <label htmlFor="message">message</label>
             <textarea
               name="message"
               id="message"
